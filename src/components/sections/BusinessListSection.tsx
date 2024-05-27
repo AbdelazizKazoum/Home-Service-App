7a8f5efe9ad9eb81101481eebcb9034b28342a5b
Abdelaziz KAZOUM <abdelaziz@KAZOUM-DEV>
2024-05-27 15:01:54 +0100

    ajust the busniess list and the categories page

--- src/components/sections/BusinessListSection.tsx
+++ src/components/sections/BusinessListSection.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button";
+import { Skeleton } from "../ui/skeleton";
 
 interface BusinessListType {
   name: string;
@@ -86,7 +87,8 @@ const data = [
 
 export const BusinessListSection = ({ category }: { category: string }) => {
   console.log("get catory :", category);
-  const [businessList, setBusinessList] = useState<BusinessListType[]>(data);
+  const [businessList, setBusinessList] = useState<BusinessListType[]>([]);
+  const [loaded, setLoaded] = useState<boolean>(false);
 
   useEffect(
     function () {
@@ -96,8 +98,11 @@ export const BusinessListSection = ({ category }: { category: string }) => {
             return item.category === category;
           })
         );
+
+        setLoaded(true);
       } else {
         setBusinessList(data);
+        setLoaded(true);
       }
     },
     [category]
@@ -110,32 +115,51 @@ export const BusinessListSection = ({ category }: { category: string }) => {
           {category ? category : "Popular Business"}
         </h1>
         <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {businessList.map((item, index) => (
-            <div className=" shadow-md rounded cursor-pointer hover:shadow-lg hover:shadow-primary ">
-              <Image
-                width={500}
-                height={200}
-                src={item.image}
-                alt={item.name}
-                className="object-cover h-[150px] md:h-[200px]  rounded  "
-              />
+          {!loaded ? (
+            <>
+              {[0, 1].map((item, id) => (
+                <div className="flex flex-col gap-3 ">
+                  <Skeleton className="h-[125px]  rounded-xl" />
+                  <div className="space-y-2">
+                    <Skeleton className="h-4" />
+                    <Skeleton className="h-4" />
+                  </div>
+                </div>
+              ))}
+            </>
+          ) : (
+            <>
+              {businessList.map((item, index) => (
+                <div className=" shadow-md rounded  hover:shadow-sm hover:shadow-primary cursor-pointer ">
+                  <Image
+                    width={500}
+                    height={200}
+                    src={item.image}
+                    alt={item.name}
+                    className="object-cover h-[150px] md:h-[200px]  rounded"
+                  />
 
-              <div className="flex flex-col items-baseline mt-2 gap-2 p-3  ">
-                <h2 className=" text-xs text-primary  bg-orange-200 rounded-full p-2     ">
-                  {item.category}
-                </h2>
-                <h2 className=" font-bold   "> {item.name} </h2>
-                <h2 className=" font-sm text-primary">
-                  {" "}
-                  {item.contactPerson}{" "}
-                </h2>
-                <h2 className=" text-xs"> {item.adress} </h2>
-                <Button variant={"default"} className="text-white">
-                  Book Now
-                </Button>
-              </div>
-            </div>
-          ))}
+                  <div className="flex flex-col items-baseline mt-2 gap-2 p-3  ">
+                    <h2 className=" text-xs text-primary  bg-orange-200 rounded-full p-2     ">
+                      {item.category}
+                    </h2>
+                    <h2 className=" font-bold   "> {item.name} </h2>
+                    <h2 className=" font-sm text-primary">
+                      {" "}
+                      {item.contactPerson}{" "}
+                    </h2>
+                    <h2 className=" text-xs"> {item.adress} </h2>
+                    <Button
+                      variant={"default"}
+                      className="text-white cursor-pointer"
+                    >
+                      Book Now
+                    </Button>
+                  </div>
+                </div>
+              ))}
+            </>
+          )}
         </div>
       </div>
     </>
